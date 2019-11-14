e6b44ecaf215d84df72754e6809f98bfc0ca19ff
koteswarkvana <koteswarao.kvana@gmail.com>
2019-11-14 12:20:49 +0530

    scrollview and flatlist functionality when user enter text in textinput and click on button showing data below.

--- App.js
+++ App.js
@@ -1,24 +1,97 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
+import { _getCurrentWatchId } from 'expo-location';
 
+
+// 
+// components and apis
+// http://facebook.github.io/react-native/docs/components-and-apis
 export default function App() {
-const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   // Hooks can only call inside the function body not inside the class.
-    return (
-      <View style={styles.container}>
-        <Text>{outputText}</Text>
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
 
-        <Button title="Change Text Content" 
-        onPress= {() => setOutputText("Hello world text changed.")}/>
+  // Here setEnteredGoal ==> setting the text from taken keyboard.
+  // Here enteredGoal ==> finally entered text from the keyboard.
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    // uncomment to see entered text in console.
+    // console.log(enteredGoal);
+    // uncomment when you use scroll view to show data.
+    // setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+
+    // Maintain key and value pair because to avoid warnings like "same text have children" 
+    setCourseGoals(currentGoals => [...currentGoals,
+      { id: Math.random().toString(), value: enteredGoal }
+    ]);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        {/* set value for text input onChangeText={goalInputHandler} */}
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        {/* print text on console while click on button */}
+        <Button title="ADD" onPress={addGoalHandler} />
+
       </View>
-    );
+      {/* <ScrollView>
+
+        {courseGoals.map((goal) => (
+          <View key={goal} style={styles.listItem}>
+
+            <Text>{goal}</Text>
+
+          </View>
+        ))}
+
+      </ScrollView> */}
+
+      {/* using flat list because it is loading ui faster than scrollview */}
+
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
+
+      <View />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  },
+  listItem: {
+    padding: 10,
+    backgroundColor: '#908970',
+    borderWidth: 1,
+    marginVertical: 10,
+    borderColor: 'black'
+  }
 });
